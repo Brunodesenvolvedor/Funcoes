b93001a6360dbd8dd5510a6fcf0a455e8c8ce25d
usertqi <emailprofissionaldobruno@gmail.com>
2026-03-24 13:07:38 -0300

    subindo validação de email

--- ValidaEmail/ValidaEmail.cs
+++ ValidaEmail/ValidaEmail.cs
@@ -43,6 +43,12 @@ public class ValidaEmail
         return _email.IndexOf('.', indexArroba) > indexArroba;
     }
     // IndexOf('@') procura a posição do primeiro @ dentro da string _email. O resultado é um número inteiro (se não existir, ele retorna -1). O método IndexOf('.', indexArroba) procura a posição do primeiro ponto (.) a partir da posição do @. Se o resultado for maior que a posição do @, significa que existe um ponto depois do @, o que é necessário para um email válido.
+
+    private bool TemPontoCom()
+    {
+        int indexPontoCom = _email.IndexOf(".com");
+        return _email.IndexOf(".com") != -1;
+    }
 }
 
 
