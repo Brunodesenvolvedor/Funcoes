bb9543dc60840931812a9ae5d3ec9c4c4e08553e
usertqi <emailprofissionaldobruno@gmail.com>
2026-03-24 16:41:20 -0300

    subindo ideias

--- ValidaEmail/ValidaEmail.cs
+++ ValidaEmail/ValidaEmail.cs
@@ -58,7 +58,25 @@ public class ValidaEmail
         return _email.IndexOf('.', indexArroba) > indexArroba;
     }
     // IndexOf('@') procura a posição do primeiro @ dentro da string _email. O resultado é um número inteiro (se não existir, ele retorna -1). O método IndexOf('.', indexArroba) procura a posição do primeiro ponto (.) a partir da posição do @. Se o resultado for maior que a posição do @, significa que existe um ponto depois do @, o que é necessário para um email válido.
+
+    private bool TextoAntesdaArroba()
+    {
+        int indexArroba = _email.IndexOf('@');
+        
+        for (int i = 0; i<indexArroba; i++)
+        {
+            if (char.IsLetter(_email[i]))
+            {
+                return true;
+            }        
+        }
+        return false;
+    }
+
+    //  e texto depois do ponto
+    //  primeiro digito n pode ser ponto ou _ ou @
+    // digito antes da arroba n pode ser ponto ou _ ou @
+    // não pode ser símbolos repetidos seguidos (ex: .. ou __ ou @@)
+
+    // depois da arroba deve ter um ponto, e depois do ponto deve ter texto com ao menos dois caracteres, não pode ter espaços em branco
 }
-
-// texto antes do @ e texto depois do ponto
-
